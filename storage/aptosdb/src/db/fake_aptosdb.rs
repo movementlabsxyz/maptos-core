aptos-labs/aptos-core

Add TransactionSummaries schema in storage

storage/aptosdb/src/db/fake_aptosdb.rs
@@ -31,6 +31,7 @@ use aptos_types::{
     contract_event::EventWithVersion,
     epoch_state::EpochState,
     event::{EventHandle, EventKey},
+    indexer::indexer_db_reader::IndexedTransactionSummary,
     ledger_info::LedgerInfoWithSignatures,
     proof::{
         accumulator::InMemoryAccumulator, position::Position, AccumulatorConsistencyProof,
@@ -742,12 +743,16 @@ impl DbReader for FakeAptosDB {
     fn get_account_ordered_transaction(
         &self,
         address: aptos_types::PeerId,
-        seq_num: u64,
+        sequence_number: u64,
         include_events: bool,
         ledger_version: Version,
     ) -> Result<Option<TransactionWithProof>> {
-        self.inner
-            .get_account_ordered_transaction(address, seq_num, include_events, ledger_version)
+        self.inner.get_account_ordered_transaction(
+            address,
+            sequence_number,
+            include_events,
+            ledger_version,
+        )
     }
 
     fn get_account_ordered_transactions(
@@ -763,6 +768,23 @@ impl DbReader for FakeAptosDB {
             seq_num,
             limit,
             include_events,
+            ledger_version,
+        )
+    }
+
+    fn get_account_all_transaction_summaries(
+        &self,
+        address: AccountAddress,
+        start_version: Option<u64>,
+        end_version: Option<u64>,
+        limit: u64,
+        ledger_version: Version,
+    ) -> Result<Vec<IndexedTransactionSummary>> {
+        self.inner.get_account_all_transaction_summaries(
+            address,
+            start_version,
+            end_version,
+            limit,
             ledger_version,
         )
     }

movementlabsxyz/maptos-core

storage/aptosdb/src/db/fake_aptosdb.rs
@@ -31,6 +31,7 @@ use aptos_types::{
     contract_event::EventWithVersion,
     epoch_state::EpochState,
     event::{EventHandle, EventKey},
+    indexer::indexer_db_reader::IndexedTransactionSummary,
     ledger_info::LedgerInfoWithSignatures,
     proof::{
         accumulator::InMemoryAccumulator, position::Position, AccumulatorConsistencyProof,
@@ -742,12 +743,16 @@ impl DbReader for FakeAptosDB {
     fn get_account_ordered_transaction(
         &self,
         address: aptos_types::PeerId,
-        seq_num: u64,
+        sequence_number: u64,
         include_events: bool,
         ledger_version: Version,
     ) -> Result<Option<TransactionWithProof>> {
-        self.inner
-            .get_account_ordered_transaction(address, seq_num, include_events, ledger_version)
+        self.inner.get_account_ordered_transaction(
+            address,
+            sequence_number,
+            include_events,
+            ledger_version,
+        )
     }
 
     fn get_account_ordered_transactions(
@@ -763,6 +768,23 @@ impl DbReader for FakeAptosDB {
             seq_num,
             limit,
             include_events,
+            ledger_version,
+        )
+    }
+
+    fn get_account_transaction_summaries(
+        &self,
+        address: AccountAddress,
+        start_version: Option<u64>,
+        end_version: Option<u64>,
+        limit: u64,
+        ledger_version: Version,
+    ) -> Result<Vec<IndexedTransactionSummary>> {
+        self.inner.get_account_transaction_summaries(
+            address,
+            start_version,
+            end_version,
+            limit,
             ledger_version,
         )
     }